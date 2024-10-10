c95c7bd65b15f0c39daa37f0b60746d49bdf554f
Jerin Jacob <82531317+jerinjacob565@users.noreply.github.com>
2024-10-10 15:55:26 +0530

    Update page.jsx

--- src/app/thankyou/page.jsx
+++ src/app/thankyou/page.jsx
@@ -6,8 +6,9 @@ import "./page.css";
 export default function ThankYou({ formData }) {
     return (<div class="container" >
         <h1>Thank You!</h1>
-        <p >Your submission has been received.</p>
-        <p>The email has been successfully sent to our customer.</p>
-
+        <p >Your submission has been received to our Dashboard.</p>
+        <p>The email has been sent sucessfully.Ask customer to check the inbox and get the valuable feedback </p>
+        <P>Made with ❤️ by Liva Kitchens And Interiors</P>
+        <marquee>If you are facing any problems in submission or email receveing,report the issue to 9778685012 </marquee>
     </div>)
 }
